9f6d6263193c1244248d528f0f79be416784084c
Eden <eden7045@gmail.com>
2021-03-19 17:09:28 +0200

    2.3 with recursive fun

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -31,92 +31,31 @@ const recurse = (str: string[], count: number, currPosition: number, output: str
 
 console.log(runLengthEncoding("aaaabbbccd")); 
 
-// {
-//     const stringArr = stringToArray(str);
-//     const output = "";
-//     const count = 0;
-//     const currPosition = 0;
-//     if(stringArr.length == 0){
-//         return output;
-//     }
-//     else{
-//     return recurse(stringArr, count, currPosition, output);
-//     }
-// };
 
-//const count = items.reduce((result, { content: { value } }) => result + value, 0)
+// /* Question 3 */
+export const isPaired = (str: string) => recursePair("", str);
 
-/* Question 3 */
-/*export const isPaired = function (str: string) {
-    const map = {
-        '(': ')',
-        '[': ']',
-        '{': '}'
-    }
-    return recursePair("", str));
-}
-const recursePair(stack: string, str: string) : boolean => {
+const recursePair=(stack: string, str: string) : boolean => {
     if(str === ""){
         return (stack === "");
     }
     else if(str.charAt(0) === '(' ||str.charAt(0) === '[' || str.charAt(0) === '{' ){
-        return recursePair(str.substring(1), stack + str.charAt(0));
+        return recursePair(str.substring(1),str.charAt(0)+stack);
     }
-    else if(str.charAt(0) === ')' ||str.charAt(0) === ']' || str.charAt(0) === '}' ) {
-        return !(stack === "") && (map(stack.charAt(0)) === str.charAt(0)) && recursePair(str.substring(1), stack.substring(1));
+    else if(str.charAt(0) === ')' || str.charAt(0) === ']' || str.charAt(0) === '}' ) {
+        return !(stack === "") && (isMatching(stack.charAt(0),str.charAt(0))) && recursePair(str.substring(1), stack.substring(1));
     }
-    else return recursePair(str.substring(1), stack);
-}
-*/
-
-export const isPaired =(str:string)=>{
-    const strArray = stringToArray(str)
-    const arr1=strArray.filter(arr => '{}'.includes(arr));
-    const arr2=strArray.filter(arr => '[]'.includes(arr));
-    const arr3=strArray.filter(arr => '()'.includes(arr));
-    const isPairedArr1=arr1.reduce(foo1,0);
-    const isPairedArr2=arr2.reduce(foo2,0);
-    const isPairedArr3=arr3.reduce(foo3,0);
-    return (isPairedArr1===0)&&(isPairedArr2===0)&&(isPairedArr3===0)
+    return recursePair(str.substring(1), stack);
+    
 }
 
-const foo1 = (counter:number, parenthesis:string)=>{
-    if(parenthesis==="{"){
-        counter = counter+1
+const isMatching = (opener: string, closer: string) : boolean => {
+    if(opener=== "{" && closer=== "}"){
+        return true;
     }
-    else if(counter>0){
-        counter = counter-1
+    if(opener=== "[" && closer=== "]"){
+        return true;
     }
-    else{
-        counter=Math.log(0)
-    }
-    return counter;
+    return false
 }
-
-const foo2 = (counter:number, parenthesis:string)=>{
-    if(parenthesis==="["){
-        counter = counter+1
-    }
-    else if(counter>0){
-        counter = counter-1
-    }
-    else{
-        counter=Math.log(0)
-    }
-    return counter;
-}
-
-const foo3 = (counter:number, parenthesis:string)=>{
-    if(parenthesis==="("){
-        counter = counter+1
-    }
-    else if(counter>0){
-        counter = counter-1
-    }
-    else{
-        counter=Math.log(0)
-    }
-    return counter;
-}
-
-console.log(isPaired("This is [some} (text)"));
+console.log(isPaired("{}"));
